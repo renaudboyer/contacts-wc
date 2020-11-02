e02db063491a63d417f0fa5e7fb992e7331eace2
Renaud BOYER <rboyer@linagora.com>
2020-11-02 18:56:14 +0100

    Add contact delete button

--- cnt-contact.js
+++ cnt-contact.js
@@ -6,8 +6,17 @@ class CntContact extends HTMLElement {
         const li = document.createElement('li');
         const slot = document.createElement('slot');
 
+        const button = document.createElement('button');
+        button.innerText = "Delete"
+        button.addEventListener("click", this.delete.bind(this))
+
         li.appendChild(slot);
+        li.appendChild(button);
         shadowRoot.appendChild(li);
+    }
+
+    delete() {
+        this.remove();
     }
 }
 
